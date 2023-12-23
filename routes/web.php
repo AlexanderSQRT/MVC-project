abe1e9ca55df20f0de10a203ece9d6595e1471ba
Al Al <dirtypurpledove@gmail.com>
2023-12-23 19:51:54 +0200

    homework 10

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+use Core\Router;
+
+Router::add(
+    'users/{id:\d+}/edit',
+    [
+        'controller' => \App\Controllers\UsersController::class,
+        'action' => 'edit',
+        'method' => 'GET'
+    ]
+);
