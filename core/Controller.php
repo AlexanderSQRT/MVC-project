96eec2f25794f26555c4fbc41d4f5c6ebab73c9c
Al Al <dirtypurpledove@gmail.com>
2024-01-07 21:22:02 +0200

    homework 11

--- core/Controller.php
+++ core/Controller.php
@@ -2,7 +2,12 @@
 
 namespace core;
 
-class Controller
+abstract class Controller
 {
+    public function before(string $action, array $params = []): bool
+    {
+        return true;
+    }
 
+    public function after(string $action) {}
 }
